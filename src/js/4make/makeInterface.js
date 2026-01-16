5db31cc8d6f6bce7611dc5ac028de07bcdaebf92
Christopher Andrew Topalian <scriptingdata@gmail.com>
2026-01-16 04:31:20 -0500

    update

--- src/js/4make/makeInterface.js
+++ src/js/4make/makeInterface.js
@@ -8,6 +8,10 @@ function makeInterface()
 
     //-//
 
+    mainDiv.append(makeTitleOfApp());
+
+    //-//
+
     let passwordTextBox = ce("textarea");
     passwordTextBox.className = "passwordTextBox";
     passwordTextBox.id = "passwordTextBox";
@@ -95,7 +99,7 @@ function makeInterface()
 //----//
 
 // Dedicated to God the Father
-// All Rights Reserved Christopher Andrew Topalian Copyright 2000-2025
+// All Rights Reserved Christopher Andrew Topalian Copyright 2000-2026
 // https://github.com/ChristopherTopalian
 // https://github.com/ChristopherAndrewTopalian
 // https://sites.google.com/view/CollegeOfScripting
--- src/js/4make/makeTitleOfApp.js
+++ src/js/4make/makeTitleOfApp.js
@@ -8,20 +8,18 @@ function makeTitleOfApp()
     titleOfApp.textContent = 'CATopalian JavaScript Password Generator';
     titleOfApp.href = 'https://github.com/ChristopherAndrewTopalian/CATopalian_JavaScript_Password_Generator';
     titleOfApp.target = '_blank';
-    titleOfApp.style.position = 'absolute';
-    titleOfApp.style.right = '20px';
-    titleOfApp.style.bottom = '10px';
+    titleOfApp.style.fontFamily = 'Arial';
     titleOfApp.style.fontSize = '17px';
     titleOfApp.style.fontWeight = 'bold';
+    titleOfApp.style.color = 'rgb(170, 170, 170)';
     titleOfApp.style.textDecoration = 'none';
-    titleOfApp.style.color = 'gray';
-    ba(titleOfApp);
+    return titleOfApp;
 }
 
 //--//
 
 // Dedicated to God the Father
-// All Rights Reserved Christopher Andrew Topalian Copyright 2000-2025
+// All Rights Reserved Christopher Andrew Topalian Copyright 2000-2026
 // https://github.com/ChristopherTopalian
 // https://github.com/ChristopherAndrewTopalian
 // https://sites.google.com/view/CollegeOfScripting
